5fd58d9f20e7090151a1ccf0e29c2da62e9590d4
Joshua Madin <jmadin@gmail.com>
2017-07-05 06:44:30 +1000

    First commit.

---
+++ R/scaf.R
@@ -0,0 +1,45 @@
+#' A Quick Project Creation Function
+#'
+#' This function allows you to rapidly create a directory structure and some essential files for an R project.
+#' @param project_name A name for your project.
+#' @keywords scafford
+#' @export
+#' @examples
+#' scaf("my_project")
+
+scaf <- function(project_name) {
+
+	project_name
+
+	project <- try(system(paste("mkdir", project_name), ignore.stderr=TRUE))
+
+	if (project == 1) {
+		cat(paste("Warning: a directory called", project_name, "already exists. Choose a different project name.\n"))
+	} else {
+
+		setwd(project_name)
+
+		system("mkdir doc")
+		system(paste0("touch doc/", project_name, ".md"))
+		system("mkdir data")
+		system("mkdir R")
+		system("mkdir output")
+		system("mkdir output/figs")
+		system("touch analysis.R")
+		system("touch R/functions.R")
+		system("touch README.md")
+		system(paste("echo '#", project_name, "' > README.md"))
+
+		cat("Project directories and files successfully created.\n")
+
+		git <- try(system("git init", ignore.stdout=TRUE))
+		if (git == 1) {
+			cat("Warning: Git could not be initiatlised. Is it installed? You can initialise Git yourself later using 'git init' in the project directory.\n")
+		} else {
+			system("touch .gitignore")
+			cat("Git succesfully initiatlised and '.gitignore' file added.\n")
+		}
+
+		cat(paste("Project", project_name, "successfully created.\n"))
+	}
+}
